7eea7a16777cbd8d4cc01a2ec7058187f198d5a1
olishkevych <olishkevych.natalia@gmail.com>
2023-04-28 19:49:45 +0200

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(event) {
+  timerId = setInterval(() => {
+    changeColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function onStopBtnClick(event) {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
