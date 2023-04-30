7fd8a14ee7f9a800468c9b3719f0074563e76a8f
olishkevych <olishkevych.natalia@gmail.com>
2023-04-30 07:15:43 +0000

    Deploying to gh-pages from @ olishkevych/goit-js-hw-09@50518d98fd0349252b724af625f8f9c8f174b6e4 🚀

---
+++ 01-color-switcher.ad19589f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(function(e){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(e){clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.ad19589f.js.map
